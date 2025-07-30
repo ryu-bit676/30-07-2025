0d262dfa54659a01a746efb3745cc0b37065c293
ryu-bit676 <achmad_mariadi_34rpl@student.smktelkom-mlg.sch.id>
2025-07-30 10:23:43 +0700

    Add files via upload

---
+++ ppepe.java
@@ -0,0 +1,20 @@
+public class ppepe {
+    public static void main(String[] args) {
+    //tipe_data nama_variabel = nilai;
+    String nama = "achmad riyu mariadi";
+    int usia = 15;
+    String nama_ayah = "aria pradesa sugiarto";
+    String nama_ibu = "rahayu oktavia";
+    String hobi = "main game";
+
+    System.out.println("Nama : " + nama);
+    System.out.println("usia : " + usia + " tahun");
+    System.out.println("nama ayah : " + nama_ayah);
+    System.out.println("nama ibu: " + nama_ibu);
+    System.out.println("hobi : " + hobi);
+
+
+    
+
+   } 
+}
